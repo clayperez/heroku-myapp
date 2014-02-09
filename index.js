84916d14dd1947646c489a9e53e26f888118d1cb
Carlos Perez <clayperez@gmail.com>
2014-02-09 00:53:57 -0800

    Added a sql query test

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ var mysql = require("mysql");
 var logfmt = require("logfmt");
 var app = express();
 
+var connection = mysql.createConnection({
+  host     : '72.3.204.212',
+  user     : '554957_results',
+  password : 'Moresby1$'
+});
+
+
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World! - '+typeof mysql);
+
+	connection.connect();
+	connection.query('USE 554957_liveresults; SELECT 1 + 1 AS solution', function(err, rows, fields) {
+	  if (err) {throw err;}
+	  res.send('Hello World! - '+rows[0].solution);
+	});
+	connection.end();
+
 });
 
 var port = Number(process.env.PORT || 5000);
